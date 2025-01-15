ee67798b23dc1af90eb828e6b2e531ba4403d143
Стафеев Дмитрий <stafeeff.dmitrij@yandex.ru>
2025-01-15 21:02:49 +0300

    perf(paginationproduct): добавить логику скрытия части страниц при пагинации
    n

--- src/features/catalog/components/PaginationProduct.tsx
+++ src/features/catalog/components/PaginationProduct.tsx
@@ -27,6 +27,9 @@ interface Props {
  */
 export const PaginationProduct: React.FC<Props> = ({ totalPages, currentPage, setCurrentPage, className }) => {
 
+	const visiblePage = [1, currentPage - 1, currentPage, currentPage + 1, totalPages];
+	let visiblePaginationEllipsis = false;
+
 	const handlePageChange = (page: number) => {
 		if (1 <= page && page <= totalPages) {
 			setCurrentPage(page);
@@ -45,31 +48,25 @@ export const PaginationProduct: React.FC<Props> = ({ totalPages, currentPage, se
 					/>
 				</PaginationItem>
 
-				{/* TODO подумать над алгоритмом вывода нумераций страниц с использованием черточек - PaginationEllipsis */}
-				{/*{[...Array(totalPages)].map((_, i) => {*/}
-				{/*	if (i + 1 === 1 || i + 1 === totalPages) {*/}
-				{/*		return <PaginationItem key={i}>*/}
-				{/*			<PaginationLink*/}
-				{/*				isActive={i + 1 === currentPage}*/}
-				{/*				onClick={() => handlePageChange(i + 1)}*/}
-				{/*			>{i + 1}</PaginationLink>*/}
-				{/*		</PaginationItem>*/}
-				{/*	} else {*/}
-				{/*		return <PaginationItem>*/}
-				{/*			<PaginationEllipsis />*/}
-				{/*		</PaginationItem>*/}
-				{/*	}})*/}
-				{/*}*/}
-
-				{/* все страницы */}
-				{[...Array(totalPages)].map((_, i) =>
-					<PaginationItem key={i}>
-						<PaginationLink
-							isActive={i + 1 === currentPage}
-							onClick={() => handlePageChange(i + 1)}
-						>{i + 1}</PaginationLink>
-					</PaginationItem>
-				)}
+				{/* страницы */}
+				{[...Array(totalPages)].map((_, i) => {
+					if (visiblePage.includes(i + 1)) {
+						visiblePaginationEllipsis = false;
+						return <PaginationItem key={i}>
+							<PaginationLink
+								isActive={i + 1 === currentPage}
+								onClick={() => handlePageChange(i + 1)}
+							>{i + 1}</PaginationLink>
+						</PaginationItem>;
+					} else if (!visiblePage.includes(i + 1) && !visiblePaginationEllipsis) {
+						visiblePaginationEllipsis = true;
+						return <PaginationItem>
+							<PaginationEllipsis/>
+						</PaginationItem>;
+					} else {
+						return null;
+					}
+				})}
 
 				{/* вперед */}
 				<PaginationItem>
